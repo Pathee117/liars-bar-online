febc7073d8df47cf9934fb1e9b826ae195f11af9
Tharindu Dilshan <patheex@gmail.com>
2025-11-28 00:43:31 +0530

    Update socket connection URL to use the production server

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,4 +1,4 @@
 import { io } from "socket.io-client";
 
-const URL = import.meta.env.VITE_SERVER_URL || "http://localhost:3001";
+const URL = import.meta.env.VITE_SERVER_URL || "http://157.245.51.151:3001";
 export const socket = io(URL, { autoConnect: true });
