b4c97728271dfc5ffa31ddc300c7117e7c9c979f
Tharindu Dilshan <patheex@gmail.com>
2025-11-28 13:26:37 +0530

    Refactor Docker setup and socket connection for production environment

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,4 +1,5 @@
 import { io } from "socket.io-client";
 
-const URL = import.meta.env.VITE_SERVER_URL || "http://157.245.51.151:3001";
-export const socket = io(URL, { autoConnect: true });
+export const socket = io(import.meta.env.VITE_SERVER_URL, {
+  transports: ["websocket", "polling"],
+});
